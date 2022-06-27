245250a16a53f54bf553a0051cb0a62ebf9a04b1
flawbert <contato.flawbertlorran@hotmail.com>
2022-06-27 14:12:36 -0300

    Carregamento do jogo
    Teste de upload do jogo de programação para internet no GitHub

---
+++ js/move.js
@@ -0,0 +1,45 @@
+//Obtém referência do elemento pardal
+const PARDAL = document.querySelector("#pardal");
+
+PARDAL.style.top = "50px";
+PARDAL.style.left = "500px";
+
+function mover(direcao){
+
+    let top = parseInt(PARDAL.style.top);
+    let left = parseInt(PARDAL.style.left);
+
+    switch(direcao){
+        case "^":
+        case 38:
+            top = top - 5; // ou top -= 5;
+            break;
+
+        case "v":
+        case 40:
+            top = top + 5; // ou top += 5;
+            break;
+
+        case "&lt;":
+        case 37: 
+            left -= 5;
+            break;
+
+        case "&gt;":
+        case 39:
+            left += 5;
+            break;
+
+        default:
+            console.log(direcao);    
+    }
+    // Atualiza os valores da posição do #pardal
+    PARDAL.style.top = top + "px";
+    PARDAL.style.left = left + "px";
+
+}
+
+document.body.onkeydown = function(tecla){
+    mover(tecla.keyCode);
+
+}
